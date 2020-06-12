d7c96ca58614ce3ab37f087c6beb99031b1732ad
Matthew Stanciu <mattbstanciu@gmail.com>
2020-06-12 17:08:12 -0400

    do things

--- pages/api/slack/message.js
+++ pages/api/slack/message.js
@@ -1,3 +1,5 @@
+import { getUserRecord, displayStreaks, accountsTable } from '../../../lib/api-utils'
+
 export default async (req, res) => {
   if (req.body.challenge) {
     res.json({ challenge: req.body.challenge })
@@ -10,16 +12,21 @@ export default async (req, res) => {
     text: `Yay! Serverless bot works!`,
     user: 'U4QAK9SRW'
   }
-  if (req.body.event.channel === 'G015WNVR1PS' && req.body.event.user !== 'U015D6A36AG') {
+  if (req.body.event.channel === 'G015WNVR1PS' && req.body.event.files && req.body.event.user !== 'U015D6A36AG') {
     console.log(req.body)
-    console.log(process.env.SLACK_BOT_TOKEN)
-    await fetch('https://slack.com/api/chat.postEphemeral', {
+    /*await fetch('https://slack.com/api/chat.postEphemeral', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
         'Authorization': `Bearer ${process.env.SLACK_BOT_TOKEN}`
       },
       body: JSON.stringify(postData)
+    })*/
+    let record = await getUserRecord(req.body.event.user)
+    let updatedStreakCount = record[0].fields['Streak Count'] + 1
+    await accountsTable.update(record[0].id, {
+      'Streak Count': updatedStreakCount
     })
+    displayStreaks(body.event.user, updatedStreakCount)
   }
 }
