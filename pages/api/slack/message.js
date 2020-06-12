56d028ced95fa15f669237d01ce033ce28e33a93
Matthew Stanciu <mattbstanciu@gmail.com>
2020-06-12 19:40:40 -0400

    Make it work!

--- pages/api/slack/message.js
+++ pages/api/slack/message.js
@@ -1,4 +1,4 @@
-import { getUserRecord, displayStreaks, accountsTable } from '../../../lib/api-utils'
+import { getUserRecord, displayStreaks, accountsTable, updatesTable, makeFilePublic } from '../../../lib/api-utils'
 
 export default async (req, res) => {
   if (req.body.challenge) {
@@ -6,16 +6,30 @@ export default async (req, res) => {
   }
   await res.json({ ok: true })
 
-  const postData = {
-    channel: 'G015WNVR1PS',
-    attachments: [],
-    text: `Yay! Serverless bot works!`,
-    user: 'U4QAK9SRW'
-  }
-  if (req.body.event.channel === 'G015WNVR1PS' && req.body.event.subtype === 'file_share' && req.body.event.user !== 'U015D6A36AG') {
-    console.log(req.body)
-    console.log(req.body.event.files)
-    /*await fetch('https://slack.com/api/chat.postEphemeral', {
+  if (req.body.event.channel === 'G015C21HR7C' && req.body.event.subtype === 'file_share' && req.body.event.user !== 'U015D6A36AG') {
+    const files = req.body.event.files
+    let attachments = []
+    const promiseArray = files.map(async file => {
+      attachments.push({
+        'url': await makeFilePublic(file.id, file.name)
+      })
+    })
+    await Promise.all(promiseArray)
+
+    const userRecord = await getUserRecord(req.body.event.user)
+    await updatesTable.create({
+      'Slack Account': [userRecord.id],
+      'Post Time': new Date().toUTCString(),
+      'Text': req.body.event.text,
+      'Attachments': attachments
+    })
+    /*const postData = {
+      channel: 'G015WNVR1PS',
+      attachments: [],
+      text: `Yay! Serverless bot works!`,
+      user: 'U4QAK9SRW'
+    }
+    await fetch('https://slack.com/api/chat.postEphemeral', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -24,11 +38,10 @@ export default async (req, res) => {
       body: JSON.stringify(postData)
     })*/
     let record = await getUserRecord(req.body.event.user)
-    console.log(record)
     let updatedStreakCount = record.fields['Streak Count'] + 1
     await accountsTable.update(record.id, {
       'Streak Count': updatedStreakCount
     })
-    displayStreaks(body.event.user, updatedStreakCount)
+    displayStreaks(req.body.event.user, updatedStreakCount)
   }
 }
