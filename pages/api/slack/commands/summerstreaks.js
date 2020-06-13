2391f5268900ad3c562db6f3869ca5727d620b8d
Lachlan Campbell <lachlan@hackclub.com>
2020-06-13 12:28:52 -0400

    A few Slack code improvements

--- pages/api/slack/commands/summerstreaks.js
+++ pages/api/slack/commands/summerstreaks.js
@@ -1,14 +1,24 @@
-import { accountsTable, displayStreaks, getUserRecord, canDisplayStreaks, sendCommandResponse } from '../../../../lib/api-utils'
+import {
+  accountsTable,
+  displayStreaks,
+  getUserRecord,
+  canDisplayStreaks,
+  sendCommandResponse
+} from '../../../../lib/api-utils'
 
 export default async (req, res) => {
   await res.status(200).end()
 
-  let record = await getUserRecord(req.body.user_id)
-  let display = await canDisplayStreaks(req.body.user_id)
-  accountsTable.update(record.id, {
-    'Display Streak': !display
-  })
-  await sendCommandResponse(req.body.response_url, display ? `Your streak count is now invisible.` : `Your streak count is now visible!`)
-  let streakCount = record.fields['Streak Count']
-  displayStreaks(req.body.user_id, streakCount)
+  const userId = req.body.user_id
+  const record = await getUserRecord(userId)
+  const display = await canDisplayStreaks(userId)
+  accountsTable.update(record.id, { 'Display Streak': !display })
+  await sendCommandResponse(
+    req.body.response_url,
+    display
+      ? `Your streak count is now invisible.`
+      : `Your streak count is now visible!`
+  )
+  const streakCount = record.fields['Streak Count']
+  displayStreaks(userId, streakCount)
 }
--- pages/api/slack/message.js
+++ pages/api/slack/message.js
@@ -1,33 +1,39 @@
-import { getUserRecord, displayStreaks, accountsTable, updatesTable, makeFilePublic, postEphemeral } from '../../../lib/api-utils'
+import {
+  getUserRecord,
+  displayStreaks,
+  accountsTable,
+  updatesTable,
+  makeFilePublic,
+  postEphemeral
+} from '../../../lib/api-utils'
 
 export default async (req, res) => {
-  if (req.body.challenge) {
-    res.json({ challenge: req.body.challenge })
-  }
+  const { challenge, event } = req.body
+
+  if (challenge) res.json({ challenge })
   await res.json({ ok: true })
 
-  if (req.body.event.channel === 'C0P5NE354' && req.body.event.subtype === 'file_share') {
-    const files = req.body.event.files
-    let attachments = []
-    const promiseArray = files.map(async file => {
-      attachments.push({
-        'url': await makeFilePublic(file.id, file.name)
+  if (event.channel === 'C0P5NE354' && event.subtype === 'file_share') {
+    const files = event.files
+    const attachments = []
+    await Promise.all(
+      files.map(async file => {
+        attachments.push({ url: await makeFilePublic(file.id, file.name) })
       })
-    })
-    await Promise.all(promiseArray)
+    )
 
-    const userRecord = await getUserRecord(req.body.event.user)
+    const userRecord = await getUserRecord(event.user)
     await updatesTable.create({
       'Slack Account': [userRecord.id],
       'Post Time': new Date().toUTCString(),
-      'Text': req.body.event.text,
-      'Attachments': attachments
+      Text: event.text,
+      Attachments: attachments
     })
-    let record = await getUserRecord(req.body.event.user)
-    let updatedStreakCount = record.fields['Streak Count'] + 1
+    const record = await getUserRecord(event.user)
+    const updatedStreakCount = record.fields['Streak Count'] + 1
     await accountsTable.update(record.id, {
       'Streak Count': updatedStreakCount
     })
-    displayStreaks(req.body.event.user, updatedStreakCount)
+    displayStreaks(event.user, updatedStreakCount)
   }
 }
