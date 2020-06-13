800adb65127d2c32d5aa1d371a8ca1387832b527
Lachlan Campbell <lachlan@hackclub.com>
2020-06-13 11:35:56 -0400

    Lazy-load images

--- components/post.js
+++ components/post.js
@@ -1,5 +1,6 @@
 import { formatDate } from '../lib/dates'
 import { filter } from 'lodash'
+import { LazyLoadImage } from 'react-lazy-load-image-component'
 import Link from 'next/link'
 
 const avatars = {
@@ -18,56 +19,57 @@ const Post = ({
   attachments = [],
   postedAt
 }) => (
-  <section className="post">
-    <Link href="/[profile]" as={`/${username}`}>
-      <a className="post-header">
-        {!profile && (
-          <>
-            <img
-              src={`https://hackclub.com/team/${avatars[username] ||
-                username}.jpg`}
-              width={48}
-              alt={username}
-              className="post-header-avatar"
+    <section className="post">
+      <Link href="/[profile]" as={`/${username}`}>
+        <a className="post-header">
+          {!profile && (
+            <>
+              <img
+                src={`https://hackclub.com/team/${avatars[username] ||
+                  username}.jpg`}
+                width={48}
+                alt={username}
+                className="post-header-avatar"
+              />
+              <strong className="post-header-name">
+                @{username}
+                {streakDisplay && (
+                  <span className="badge post-header-streak">{streakCount}</span>
+                )}
+              </strong>
+            </>
+          )}
+          <time className="post-header-date" datetime={postedAt}>
+            {formatDate(postedAt)}
+          </time>
+        </a>
+      </Link>
+      <p className="post-text">{text}</p>
+      {attachments.length > 0 && (
+        <div className="post-attachments">
+          {filter(attachments, a => a.type.startsWith('image')).map(img => (
+            <a
+              href={img.thumbnails.full.url}
+              target="_blank"
+              className="post-attachment"
+            >
+              <LazyLoadImage alt={img.filename} effect="blur"
+                src={img.thumbnails.large.url} />
+            </a>
+          ))}
+          {filter(attachments, a => a.type.startsWith('video')).map(vid => (
+            <video
+              controls
+              alt={vid.filename}
+              src={vid.url}
+              onMouseOver={event => event.target.play()}
+              onMouseOut={event => event.target.pause()}
+              className="post-attachment"
             />
-            <strong className="post-header-name">
-              @{username}
-              {streakDisplay && (
-                <span className="badge post-header-streak">{streakCount}</span>
-              )}
-            </strong>
-          </>
-        )}
-        <time className="post-header-date" datetime={postedAt}>
-          {formatDate(postedAt)}
-        </time>
-      </a>
-    </Link>
-    <p className="post-text">{text}</p>
-    {attachments.length > 0 && (
-      <div className="post-attachments">
-        {filter(attachments, a => a.type.startsWith('image')).map(img => (
-          <a
-            href={img.thumbnails.full.url}
-            target="_blank"
-            className="post-attachment"
-          >
-            <img alt={img.filename} src={img.thumbnails.large.url} />
-          </a>
-        ))}
-        {filter(attachments, a => a.type.startsWith('video')).map(vid => (
-          <video
-            controls
-            alt={vid.filename}
-            src={vid.url}
-            onMouseOver={event => event.target.play()}
-            onMouseOut={event => event.target.pause()}
-            className="post-attachment"
-          />
-        ))}
-      </div>
-    )}
-  </section>
-)
+          ))}
+        </div>
+      )}
+    </section>
+  )
 
 export default Post
--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import NextApp from 'next/app'
 
 import '@hackclub/theme/fonts/reg-bold.css'
 import '../public/app.css'
+import 'react-lazy-load-image-component/src/effects/blur.css'
 import Flag from '../components/flag'
 
 export default class App extends NextApp {
