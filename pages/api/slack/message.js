e294fa09568fcf9437d1e73a45c59fa951525e78
Zach Latta <zach@zachlatta.com>
2020-06-14 15:57:44 -0400

    React to updates

--- pages/api/slack/message.js
+++ pages/api/slack/message.js
@@ -6,11 +6,26 @@ import {
   getPublicFileUrl
 } from '../../../lib/api-utils'
 
+// ex. react('add', 'C248d81234', '12384391.12231', 'beachball')
+async function react(addOrRemove, channel, ts, reaction) {
+  return fetch('https://slack.com/api/reactions.' + addOrRemove, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${process.env.SLACK_BOT_TOKEN}`
+    },
+    body: JSON.stringify({ channel: channel, name: reaction, timestamp: ts })
+  }).then(r => r.json())
+}
+
 export default async (req, res) => {
   const { challenge, event } = req.body
 
-  if (challenge) res.json({ challenge })
-  await res.json({ ok: true })
+  if (challenge) {
+    return await res.json({ challenge })
+  } else {
+    res.json({ ok: true })
+  }
 
   if (!((event.channel === process.env.CHANNEL || event.channel === 'G015C21HR7C' || event.channel === 'G015WNVR1PS') && event.subtype === 'file_share')) {
     console.log("Event channel", event.channel, "did not match", process.env.CHANNEL + ". Skipping event...")
@@ -18,6 +33,12 @@ export default async (req, res) => {
   }
 
   console.log("Event channel", event.channel, "matched", process.env.CHANNEL + ". Continuing...")
+
+  let resp = await react('add', event.channel, event.ts, 'beachball')
+  if (!resp.ok) {
+    console.log("Error adding reaction to shared update, not continuing:", resp)
+    return
+  }
 
   const files = event.files
 
@@ -54,4 +75,10 @@ export default async (req, res) => {
   })
 
   await displayStreaks(event.user, updatedStreakCount)
+
+  // remove beachball react, add final summer-of-making react
+  await Promise.all([
+    react('remove', event.channel, event.ts, 'beachball'),
+    react('add', event.channel, event.ts, 'summer-of-making')
+  ])
 }
