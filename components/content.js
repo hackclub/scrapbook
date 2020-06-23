b8f8a6a90042728d26718f599f538e71b0a84ce3
frencojobs <frenco.willian@gmail.com>
2020-06-23 08:41:41 +0630

    Add emoji rendering

--- components/content.js
+++ components/content.js
@@ -2,6 +2,21 @@
 import { memo } from 'react'
 import { last } from 'lodash'
 import Mention from './mention'
+import { toArray } from 'react-emoji-render'
+
+const parseEmojis = (value) => {
+  const emojisArray = toArray(value)
+
+  const parsedString = emojisArray.reduce((previous, current) => {
+    if (typeof current === 'string') {
+      return previous + current
+    } else {
+      return previous + current.props.children
+    }
+  }, '')
+
+  return parsedString
+}
 
 export const mapLinks = (text, link, mention) =>
   text.split(/(<.+?\|?\S+>)|(@\S+)/).map((chunk, i) => {
@@ -22,13 +37,15 @@ export const mapLinks = (text, link, mention) =>
 const Content = memo(({ children }) => (
   <p className="post-text">
     {mapLinks(
-      children,
+      parseEmojis(children),
       (url, children, i) => (
         <a href={url} target="_blank" key={i}>
           {children}
         </a>
       ),
-      (username, i) => <Mention username={username} key={username + i} />
+      (username, i) => (
+        <Mention username={username} key={username + i} />
+      )
     )}
   </p>
 ))
