9e4ca4913b7688c7d23ac30b9bbec99e748391e9
Lachlan Campbell <lachlan@hackclub.com>
2020-07-01 18:58:45 +0000

    Add rich text formatting

--- components/content.js
+++ components/content.js
@@ -3,36 +3,45 @@ import { memo } from 'react'
 import { last } from 'lodash'
 import Mention from './mention'
 
-export const mapLinks = (text, link, mention) =>
-  text.split(/(<.+?\|?\S+>)|(@\w+)/).map((chunk, i) => {
-    if (chunk?.startsWith('@')) {
-      return mention(chunk.replace('@', ''), i)
-    }
-    if (chunk?.startsWith('<')) {
-      const parts = chunk.match(/<((.+)\|)?(\S+)>/)
-      const url = parts?.[2] || last(parts)
-      const children = last(parts)
-        ?.replace(/https?:\/\//, '')
-        .replace(/\/$/, '')
-      return link(url, children, i)
-    }
-    return chunk?.replace('&amp;', '&')
-  })
+export const formatText = text =>
+  text
+    .split(
+      /(<.+?\|?\S+>)|(@\w+)|(`{3}[\S\s]+`{3})|(`[^`]+`)|(_[^_]+_)|(\*[^\*]+\*)/
+    )
+    .map((chunk, i) => {
+      if (chunk?.startsWith('@')) {
+        const username = chunk.replace('@', '')
+        return <Mention username={username} key={username + i} />
+      }
+      if (chunk?.startsWith('<')) {
+        const parts = chunk.match(/<((.+)\|)?(\S+)>/)
+        const url = parts?.[2] || last(parts)
+        const children = last(parts)
+          ?.replace(/https?:\/\//, '')
+          .replace(/\/$/, '')
+        return (
+          <a href={url} target="_blank" key={i}>
+            {children}
+          </a>
+        )
+      }
+      if (chunk?.startsWith('```')) {
+        return <pre key={i}>{chunk.replace(/```/g, '')}</pre>
+      }
+      if (chunk?.startsWith('`')) {
+        return <code key={i}>{chunk.replace(/`/g, '')}</code>
+      }
+      if (chunk?.startsWith('*')) {
+        return <strong key={i}>{chunk.replace(/\*/g, '')}</strong>
+      }
+      if (chunk?.startsWith('_')) {
+        return <i key={i}>{chunk.replace(/_/g, '')}</i>
+      }
+      return chunk?.replace('&amp;', '&')
+    })
 
 const Content = memo(({ children }) => (
-  <p className="post-text">
-    {mapLinks(
-      children,
-      (url, children, i) => (
-        <a href={url} target="_blank" key={i}>
-          {children}
-        </a>
-      ),
-      (username, i) => (
-        <Mention username={username} key={username + i} />
-      )
-    )}
-  </p>
+  <p className="post-text">{formatText(children)}</p>
 ))
 
 export default Content
