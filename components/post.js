96be57a0ca307087438e8981f1631c74e3425e2c
Neel Redkar <neel.redkar@gmail.com>
2020-07-05 10:00:04 -0700

    delete package.lock, and indentation

--- components/post.js
+++ components/post.js
@@ -1,10 +1,10 @@
 import { convertTimestampToDate } from '../lib/dates'
 import { filter } from 'lodash'
-import { LazyLoadImage } from 'react-lazy-load-image-component'
 import Icon from '@hackclub/icons'
 import Link from 'next/link'
 import Content from './content'
 import Video from './video'
+import Image from './image'
 
 const Post = ({
   id = new Date().toISOString(),
@@ -22,100 +22,100 @@ const Post = ({
   scrollPosition,
   muted = false
 }) => (
-    <section
-      className="post"
-      id={id}
-      style={muted ? { opacity: muted, pointerEvents: 'none' } : null}
-    >
-      <Link href="/[profile]" as={`/${user.username}`}>
-        <a className="post-header">
-          {profile ? (
-            <time className="post-header-date" dateTime={postedAt}>
-              {postedAt?.startsWith('20')
-                ? convertTimestampToDate(postedAt)
-                : postedAt}
-            </time>
-          ) : (
-              <>
-                {user.avatar && (
-                  <img
-                    loading="lazy"
-                    src={user.avatar}
-                    width={48}
-                    alt={user.username}
-                    className="post-header-avatar"
+  <section
+    className="post"
+    id={id}
+    style={muted ? { opacity: muted, pointerEvents: 'none' } : null}
+  >
+    <Link href="/[profile]" as={`/${user.username}`}>
+      <a className="post-header">
+        {profile ? (
+          <time className="post-header-date" dateTime={postedAt}>
+            {postedAt?.startsWith('20')
+              ? convertTimestampToDate(postedAt)
+              : postedAt}
+          </time>
+        ) : (
+          <>
+            {user.avatar && (
+              <img
+                loading="lazy"
+                src={user.avatar}
+                width={48}
+                height={48}
+                alt={user.username}
+                className="post-header-avatar"
+              />
+            )}
+            <div className="post-header-container">
+              <span className="post-header-name">
+                <strong>@{user.username}</strong>
+                <span
+                  className={`badge post-header-streak ${
+                    user.streakCount === 0 ? 'header-streak-zero' : ''
+                  }`}
+                  title={`${user.streakCount}-day streak`}
+                >
+                  {`${user.streakCount <= 7 ? user.streakCount : '7+'}`}
+                  <Icon size={24} glyph="admin-badge" title="Streak icon" />
+                </span>
+                {user.css && (
+                  <Icon
+                    size={24}
+                    glyph="rep"
+                    title="Has a customized profile"
+                    className="post-header-css"
                   />
                 )}
-                <div className="post-header-container">
-                  <span className="post-header-name">
-                    <strong>@{user.username}</strong>
-                    <span
-                      className={`badge post-header-streak ${
-                        user.streakCount === 0 ? 'header-streak-zero' : ''
-                        }`}
-                      title={`${user.streakCount}-day streak`}
-                    >
-                      {`${user.streakCount <= 7 ? user.streakCount : '7+'}`}
-                      <Icon size={24} glyph="admin-badge" title="Streak icon" />
-                    </span>
-                    {user.css && (
-                      <Icon
-                        size={24}
-                        glyph="rep"
-                        title="Has a customized profile"
-                        className="post-header-css"
-                      />
-                    )}
-                  </span>
-                  <time className="post-header-date" dateTime={postedAt}>
-                    {postedAt?.startsWith('20')
-                      ? convertTimestampToDate(postedAt)
-                      : postedAt}
-                  </time>
-                </div>
-              </>
-            )}
-        </a>
-      </Link>
-      <Content>{text}</Content>
-      {attachments.length > 0 && (
-        <div className="post-attachments">
-          {filter(attachments, a => a?.type?.toString().startsWith('image')).map(
-            img => (
-              <a
-                key={img.url}
-                href={img.thumbnails?.full?.url || img.url}
-                target="_blank"
-                className="post-attachment"
-              >
-                <LazyLoadImage
-                  alt={img.filename}
-                  effect="blur"
-                  src={img.thumbnails?.large?.url || img.url}
-                  placeholderSrc={img.thumbnails?.small?.url}
-                  scrollPosition={scrollPosition}
-                  visibleByDefault
-                />
-              </a>
-            )
-          )}
-          {filter(attachments, a => a?.type?.toString().startsWith('audio')).map(
-            aud => (
-              <audio
-                key={aud.url}
-                className="post-attachment"
-                src={aud.url}
-                controls
-                preload="metadata"
+              </span>
+              <time className="post-header-date" dateTime={postedAt}>
+                {postedAt?.startsWith('20')
+                  ? convertTimestampToDate(postedAt)
+                  : postedAt}
+              </time>
+            </div>
+          </>
+        )}
+      </a>
+    </Link>
+    <Content>{text}</Content>
+    {attachments.length > 0 && (
+      <div className="post-attachments">
+        {filter(attachments, a => a?.type?.toString().startsWith('image')).map(
+          img => (
+            <a
+              key={img.url}
+              href={img.thumbnails?.full?.url || img.url}
+              target="_blank"
+              className="post-attachment"
+            >
+              <Image
+                alt={img.filename}
+                src={img.thumbnails?.large?.url || img.url}
+                placeholderSrc={img.thumbnails?.small?.url || img.url}
+                width={img.thumbnails?.large?.width}
+                height={img.thumbnails?.large?.height}
               />
-            )
-          )}
-          {mux.map(id => (
-            <Video key={id} mux={id} />
-          ))}
-        </div>
-      )}
-    </section>
-  )
+            </a>
+          )
+        )}
+        {filter(attachments, a => a?.type?.toString().startsWith('audio')).map(
+          aud => (
+            <audio
+              key={aud.url}
+              className="post-attachment"
+              src={aud.url}
+              controls
+              preload="metadata"
+            />
+          )
+        )}
+        {mux.map(id => (
+          <Video key={id} mux={id} />
+        ))}
+      </div>
+    )}
+  </section>
+)
 
 export default Post
