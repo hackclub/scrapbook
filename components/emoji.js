30acf9cd1bfa6a73e96ca30a430e0d32bd674372
Neel Redkar <neel.redkar@outlook.com>
2020-07-09 08:40:39 -0700

    Add custom emoji support (#37)
    * add emoji's
    
    * Delete error Alert
    
    * delete package.lock, and indentation
    
    * memoize the emoji component

---
+++ components/emoji.js
@@ -0,0 +1,30 @@
+import { memo, useState, useEffect } from 'react'
+import { LazyLoadImage } from 'react-lazy-load-image-component'
+
+const Emoji = memo(({ name }) => {
+    let [image, setImage] = useState()
+    useEffect(() => {
+        try {
+            fetch("https://emoji-ranker.now.sh/custompng")
+                .then(r => r.json())
+                .then(emojis => {
+                    if (emojis[name.replaceAll(":", "")]) {
+                        setImage(emojis[name.replaceAll(":", "")])
+                        return
+                    }
+                    setImage("https://emoji.slack-edge.com/T0266FRGM/parrot/c9f4fddc5e03d762.gif")
+                })
+        } catch (e) { }
+    }, [])
+    return (
+        <LazyLoadImage
+            alt={name}
+            effect="blur"
+            src={image}
+            height="18px"
+            visibleByDefault
+        />
+    )
+})
+
+export default Emoji
