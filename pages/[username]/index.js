927854be9fbb5e02158900ff73bcbc6abe36aebd
Lachlan Campbell <lachlan@hackclub.com>
2020-10-31 06:50:19 +0000

    Use native ISO formatting

--- pages/[username]/index.js
+++ pages/[username]/index.js
@@ -22,12 +22,7 @@ const HOST =
 const Tooltip = dynamic(() => import('react-tooltip'), { ssr: false })
 
 // Calculate heatmap date range
-const dateString = dt =>
-  [
-    dt.getFullYear(),
-    String(dt.getMonth() + 1).padStart(2, '0'),
-    String(dt.getDate()).padStart(2, '0')
-  ].join('-')
+const dateString = dt => dt.toISOString().substring(0, 10)
 const dt = new Date()
 const todayString = dateString(dt)
 const startDateString = dateString(dt.setDate(dt.getDate() - 63))
