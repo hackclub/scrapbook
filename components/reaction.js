7e785204cedb370d1893e5a4aed0c9472ed16efd
Lachlan Campbell <lachlan@hackclub.com>
2020-11-02 13:15:23 -0500

    Make reaction images responsive?

--- components/reaction.js
+++ components/reaction.js
@@ -3,9 +3,9 @@ import { EmojiImg } from './emoji'
 import { startCase } from 'lodash'
 
 const Reaction = ({ name, char, url }) => (
-  <Link href={`/r/${name}`} prefetch={false}>
+  <Link href={`/r/${name}`}>
     <a className="post-reaction" title={startCase(name)}>
-      {url && <EmojiImg src={url} name={name} width={24} height={24} />}
+      {url && <EmojiImg src={url} name={name} width={24} height={24} layout="responsive" />}
       {char}
     </a>
   </Link>
