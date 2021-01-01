535fa2de7e13c5da0d3a8e03804ad36979ddd7e7
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-01-01 17:32:11 +0800

    Vertically Align Emoji (#145)
    * Vertically Align Emoji
    
    * Fix Reactions
    
    * Fixed!

--- components/emoji.js
+++ components/emoji.js
@@ -3,14 +3,21 @@ import { stripColons } from '../lib/emoji'
 import Image from 'next/image'
 
 export const EmojiImg = ({ name, ...props }) => (
-  <Image
-    alt={name + ' emoji'}
-    loading="lazy"
-    className="post-emoji"
-    width={18}
-    height={18}
-    {...props}
-  />
+  <div
+    style={{
+      height: !props.height ? '18px' : `${props.height}px`,
+      verticalAlign: 'middle'
+    }}
+  >
+    <Image
+      alt={name + ' emoji'}
+      loading="lazy"
+      className="post-emoji"
+      width={18}
+      height={18}
+      {...props}
+    />
+  </div>
 )
 
 const CustomEmoji = memo(({ name }) => {
