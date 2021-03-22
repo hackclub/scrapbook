96465d6f0f2e37de903ee8494ad6fa32b4cfecef
Max Wofford <max@maxwofford.com>
2021-03-22 15:07:35 -0400

    Remove Fathom analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,33 +1,17 @@
-import React, { useEffect } from 'react'
-import { useRouter } from 'next/router'
-import NextApp from 'next/app'
+import React from 'react'
 
 import '../public/fonts.css'
 import '../public/app.css'
 import '../public/themes/default.css'
 import Nav from '../components/nav'
-import * as Fathom from 'fathom-client'
 import NProgress from '../components/nprogress'
 
-const App = ({ Component, pageProps }) => {
-  const router = useRouter()
-
-  useEffect(() => {
-    Fathom.load('LQTGBZMY', { includedDomains: ['scrapbook.hackclub.com'] })
-    const onRouteChangeComplete = () => Fathom.trackPageview()
-    router.events.on('routeChangeComplete', onRouteChangeComplete)
-    return () => {
-      router.events.off('routeChangeComplete', onRouteChangeComplete)
-    }
-  }, [])
-
-  return (
-    <>
-      <Nav />
-      <NProgress color={'#ec3750'} />
-      <Component {...pageProps} />
-    </>
-  )
-}
+const App = ({ Component, pageProps }) => (
+  <>
+    <Nav />
+    <NProgress color={'#ec3750'} />
+    <Component {...pageProps} />
+  </>
+)
 
 export default App
