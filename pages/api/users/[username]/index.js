6b3e44f72e35a1d587e8ed1d3b740a15c3018f68
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-08-10 02:04:01 +0800

    /api/users/[username]/index!

--- pages/api/users/[username]/index.js
+++ pages/api/users/[username]/index.js
@@ -1,56 +1,57 @@
 import { map, find, isEmpty, orderBy } from 'lodash'
 import { getRawUsers } from '../index'
 import { getRawPosts, transformPost } from '../../posts'
+import prisma from '../../../../lib/prisma'
 
-export const getProfile = async (value, field = 'Username') => {
-  const opts = JSON.stringify({
-    maxRecords: 1,
-    filterByFormula: `${
-      field === 'id' ? 'RECORD_ID()' : `{${field}}`
-    } = "${value}"`
-  })
-  const user = await fetch(
-    `https://airbridge.hackclub.com/v0.1/Summer%20of%20Making%20Streaks/Slack%20Accounts?select=${opts}`
-  )
-    .then(r => r.json())
-    .then(a => (Array.isArray(a) ? a[0] : null))
+export const getProfile = async (value, field = 'username') => {
+  let where = {}
+  where[field] = value
+  const opts = {
+    where
+  }
+  console.log(opts)
+  const user = await prisma.accounts.findFirst(opts)
+  console.log(user)
   if (!user) console.error('Could not fetch account', value)
   return user && user?.username ? user : {}
 }
 
 export const getPosts = async user => {
   const allUpdates = await getRawPosts(null, {
-    filterByFormula: `{Username} = "${user.username}"`
+    where: {
+      accountsSlackID: user.slackID
+    }
   })
 
   if (!allUpdates) console.error('Could not fetch posts')
-  return allUpdates.map(({ id, fields }) => transformPost(id, fields))
+  return allUpdates.map(p => transformPost(p))
 }
 
 export const getMentions = async user => {
   const users = await getRawUsers(true)
   const allUpdates = await getRawPosts(null, {
-    filterByFormula: `IF(FIND("@${user.username}",{text})>0,TRUE(),FALSE())`
+    where: {
+      text: { contains: `@${user.username}` }
+    }
   })
   if (!allUpdates) console.error('Could not fetch posts')
   return allUpdates
     .map(p => {
-      const user = find(users, { id: p.fields['Slack Account']?.[0] }) || {}
-      p.user = user.id ? user : null
+      p.user = find(users, { slackID: p.accountsSlackID }) || {}
       return p
     })
     .filter(p => !isEmpty(p.user))
-    .map(({ id, user, fields }) => transformPost(id, fields, user))
+    .map(p => transformPost(p))
 }
 
 export default async (req, res) => {
   const profile = await getProfile(req.query.username)
-  if (!profile?.id)
+  if (!profile?.slackID)
     return res.status(404).json({ status: 404, error: 'Cannot locate user' })
   let webring = []
   if (profile.webring) {
     webring = await Promise.all(
-      profile.webring.map(async id => await getProfile(id, 'id'))
+      profile.webring.map(async id => await getProfile(id, 'slackID'))
     )
   }
   const posts = (await getPosts(profile)) || []
