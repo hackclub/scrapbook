97091459cf2836a9404d9de38edf107ddf1831f9
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-08-10 01:39:20 +0800

    Deprecate getProfiles

--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -3,6 +3,4 @@ import prisma from '../../../lib/prisma'
 export const getRawUsers = (onlyFull = false) =>
   prisma.accounts.findMany(onlyFull ? { where: { fullSlackMember: true } } : {})
 
-export const getProfiles = () => getRawUsers()
-
-export default async (req, res) => getProfiles().then(u => res.json(u || []))
+export default async (req, res) => getRawUsers().then(u => res.json(u || []))
