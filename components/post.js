981d8689db7163a1a25a4745f221a856f270a8de
Max Wofford <max@maxwofford.com>
2022-02-08 18:22:40 -0500

    Add name & version of cartridges, error handling

--- components/post.js
+++ components/post.js
@@ -125,7 +125,6 @@ const Post = ({
         </a>
       </Link>
     )}
-    <Content>{text}</Content>
     {(text) && (<Cartridges text={text} />)}
     {(attachments.length > 0 || mux.length > 0) && (
       <div className="post-attachments">
