6c16d33cc24eb5ea556c2c32f3bc7619377bf645
Max Wofford <max@maxwofford.com>
2022-02-11 16:13:34 -0500

    Fetch real gamelab score data

--- pages/api/cartridges/[id]/scores.js
+++ pages/api/cartridges/[id]/scores.js
@@ -1,11 +1,7 @@
-const getScoreboard = async ({id}) => {
+const getScoreboard = async (id) => {
   // placeholder values while testing
-  const data = {
-    "U891739": { "user": "n9k", "score": 29 },
-    "U6383G7": { "user": "ced", "score": 5 },
-  }
-  // const url = `https://misguided.enterprises/gamelabscores/${id}`
-  // const data = await fetch(url).then(r => r.json())
+  const url = `https://misguided.enterprises/gamelabscores/${id}`
+  const data = await fetch(url).then(r => r.json())
   const scores = Object.entries(data)
   const sortedScores = scores.sort(([ , a], [ , b]) => b.score - a.score)
   const topScores = sortedScores.splice(0, 3)
