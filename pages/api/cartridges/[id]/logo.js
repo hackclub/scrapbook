e0c057231d1474d58d13e12e352325ebca050096
Max Wofford <max@maxwofford.com>
2022-04-06 00:18:58 -0400

    If no logo, fallback to sprite

--- pages/api/cartridges/[id]/logo.js
+++ pages/api/cartridges/[id]/logo.js
@@ -3,9 +3,10 @@ import { getCartridge } from "."
 export default async (req, res) => {
   const cartridge = await getCartridge(req.query.id)
   
-  const spriteData = cartridge?.assets?.find(asset => (
-    asset?.type == 'sprite' && asset?.name == 'logo'
+  const sprites = cartridge?.assets?.filter(asset => (
+    asset?.type == 'sprite'
   ))
+  const spriteData = sprites.find(asset => asset?.name == 'logo') || sprites[0]
 
   if (spriteData) {
     res.send(spriteData)
