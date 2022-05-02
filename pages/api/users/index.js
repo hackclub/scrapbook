e9bfafe75814791a29ef3ca4e323cb0ee54a721c
Sam Poder <39828164+sampoder@users.noreply.github.com>
2022-05-02 09:47:34 +0800

    Update index.js

--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -1,9 +1,7 @@
 import prisma from '../../../lib/prisma'
 
 export const getRawUsers = (onlyFull = false, where = undefined) =>
-  prisma.accounts.findMany({
-    where: null
-  })
+  prisma.accounts.findMany({where})
 
 // Find users with at least one Scrapbook post
 export default async (req, res) => getRawUsers(
