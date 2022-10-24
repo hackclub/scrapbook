0d77bbd99fdf53961457bc965331c79f1c06c33d
Reese Armstrong <me@reeseric.ci>
2022-10-24 18:50:49 -0500

    Update tailwind.config.js (#388)

--- web/config/tailwind.config.js
+++ web/config/tailwind.config.js
@@ -29,9 +29,9 @@ module.exports = {
     },
     fontFamily: {
       'sans': `'Baloo 2', system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, sans-serif'`
-      'mono': 'SF Mono', 'Roboto Mono', Menlo, Consolas, monospace,
-      'header': 'Shrikhand', system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, sans-serif
-  }
+      'mono': `'SF Mono', 'Roboto Mono', Menlo, Consolas, monospace`,
+      'header': `'Shrikhand', system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, sans-serif`
+    }
   },
   plugins: [],
 }
