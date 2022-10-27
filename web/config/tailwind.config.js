f341cc0f1372ed6eddabf04e59a8b210bafcbc11
Sam Poder <sam@hackclub.com>
2022-10-27 22:05:25 +0800

    🫕 Font Arrays

--- web/config/tailwind.config.js
+++ web/config/tailwind.config.js
@@ -28,7 +28,7 @@ module.exports = {
       progress: '#ec3750',
     },
     fontFamily: {
-      'sans': `Baloo 2, system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, sans-serif`,
+      'sans': ["Baloo 2", "system-ui", "-apple-system", "BlinkMacSystemFont", "'Segoe UI'", "Roboto", "sans-serif"],
       'mono': `'SF Mono', 'Roboto Mono', Menlo, Consolas, monospace`,
       'header': `'Shrikhand', system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, sans-serif`
     }
