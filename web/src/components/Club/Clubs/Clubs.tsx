7f23d9fdccb0821d6a934ae3fd29bc0badaa8e28
Charalampos Fanoulis <yo@fanoulis.dev>
2022-11-30 00:17:22 +0200

    style: add some spice to clubs overview
    to add: viewing clubs you belong, somehow manage getting clubs deleted...

--- web/src/components/Club/Clubs/Clubs.tsx
+++ web/src/components/Club/Clubs/Clubs.tsx
@@ -1,23 +1,10 @@
+import type { FindClubs } from 'types/graphql'
+
 import { Link, routes } from '@redwoodjs/router'
-import { useMutation } from '@redwoodjs/web'
-import { toast } from '@redwoodjs/web/toast'
-import { useAuth } from '@redwoodjs/auth'
-
-import { QUERY } from 'src/components/Club/ClubsCell'
-
-import type { DeleteClubMutationVariables, FindClubs } from 'types/graphql'
-
-const DELETE_CLUB_MUTATION = gql`
-  mutation DeleteClubMutation($id: String!) {
-    deleteClub(id: $id) {
-      id
-    }
-  }
-`
 
 const MAX_STRING_LENGTH = 150
 
-const truncate = (value: string | number) => {
+const _truncate = (value: string | number) => {
   const output = value?.toString()
   if (output?.length > MAX_STRING_LENGTH) {
     return output.substring(0, MAX_STRING_LENGTH) + '...'
@@ -26,88 +13,69 @@ const truncate = (value: string | number) => {
 }
 
 const ClubsList = ({ clubs }: FindClubs) => {
-  const [deleteClub] = useMutation(DELETE_CLUB_MUTATION, {
-    onCompleted: () => {
-      toast.success('Club deleted')
-    },
-    onError: (error) => {
-      toast.error(error.message)
-    },
-    // This refetches the query on the list page. Read more about other ways to
-    // update the cache over here:
-    // https://www.apollographql.com/docs/react/data/mutations/#making-all-other-cache-updates
-    refetchQueries: [{ query: QUERY }],
-    awaitRefetchQueries: true,
-  })
-
-  const onDeleteClick = (id: DeleteClubMutationVariables['id']) => {
-    if (confirm('Are you sure you want to delete club ' + id + '?')) {
-      deleteClub({ variables: { id } })
-    }
-  }
-
-  const { currentUser } = useAuth()
+  // const { currentUser } = useAuth()
 
   return (
     <>
-      {' '}
-      <div className="rw-segment rw-table-wrapper-responsive">
-        <table className="rw-table">
-          <thead>
-            <tr>
-              <th>Logo</th>
-              <th>Name</th>
-              <th>&nbsp;</th>
-            </tr>
-          </thead>
-          <tbody>
-            {clubs.map((club) => (
-              <tr key={club.id}>
-                <td>
-                  <img src={club.logo} className="h-32 rounded" />
-                </td>
-                <td>{truncate(club.name)}</td>
-                <td>
-                  <nav className="rw-table-actions">
-                    <Link
-                      to={routes.club({ slug: club.slug })}
-                      title={'Show club ' + club.slug + ' detail'}
-                      className="rw-button rw-button-small"
-                    >
-                      Show
-                    </Link>
-                    {club.members
-                      .filter((x) => x.admin)
-                      .map((x) => x.accountId)
-                      .includes(currentUser.id) && (
-                      <>
-                        <Link
-                          to={routes.editClub({ id: club.id })}
-                          title={'Edit club ' + club.id}
-                          className="rw-button rw-button-small rw-button-blue"
-                        >
-                          Edit
-                        </Link>
-                        <button
-                          type="button"
-                          title={'Delete club ' + club.id}
-                          className="rw-button rw-button-small rw-button-red"
-                          onClick={() => onDeleteClick(club.id)}
-                        >
-                          Delete
-                        </button>
-                      </>
-                    )}
-                  </nav>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-      <Link to={routes.newClub()} className="rw-button rw-button-green mt-3">
-        <div className="rw-button-icon">+</div> New Club
-      </Link>
+      <main
+        className="masonary"
+        style={{ height: `${1000 * Math.floor((clubs.length + 1) / 3) + 1}px` }}
+      >
+        <div
+          key="newclub_noid"
+          className="masonary-item height-100 flex flex-col rounded-md border border-sunken bg-background p-3"
+        >
+          <div className="flex flex-row">
+            <div className="mr-4 flex h-24 w-24 items-center justify-center bg-gradient-to-tl from-green to-blue text-5xl font-black text-white">
+              +
+            </div>
+            <div>
+              <span className="font-xs font-semibold italic text-purple">
+                @newclub
+              </span>
+              <h3 className="text-2xl font-bold">Your club name here</h3>
+              <p>Scrapook&apos;s better with friends. Make a club now!</p>
+            </div>
+          </div>
+          <Link
+            to={routes.newClub()}
+            className="text-right font-bold text-slate"
+          >
+            NEW CLUB, NOW!
+          </Link>
+        </div>
+        {clubs.map((club) => (
+          <div
+            key={club.id}
+            className="masonary-item height-100 flex flex-col rounded-md border border-sunken bg-background p-3"
+          >
+            <div className="flex flex-row">
+              <img
+                className="mr-4 h-24 w-24"
+                src={club.logo}
+                alt={`${club.name}'s logo`}
+              />
+              <div>
+                <span className="font-xs font-semibold italic text-purple">
+                  @{club.slug}
+                </span>
+                <h3 className="text-2xl font-bold">{club.name}</h3>
+                <p>
+                  Nulla vel metus dui. Proin eu eleifend elit. Praesent a enim
+                  nec est varius interdum at ac eros. Sed id sollicitudin augue,
+                  eu placerat nibh.
+                </p>
+              </div>
+            </div>
+            <Link
+              to={routes.club({ slug: club.slug })}
+              className="text-right font-bold text-slate"
+            >
+              SEE MORE
+            </Link>
+          </div>
+        ))}
+      </main>
     </>
   )
 }
