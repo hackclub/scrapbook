62474286bf9580293b44264814508b659f653875
Sam Poder <sam@hackclub.com>
2022-12-17 20:42:02 +0800

    Banners for all!

--- pages/r/[emoji].js
+++ pages/r/[emoji].js
@@ -72,6 +72,7 @@ const Header = ({ name, url, char }) => (
           .epoch-banner {
             margin-bottom: 16px;
             padding: 16px;
+            padding-top: 24px;
             background-size: cover;
             background-position: center;
             background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.5) 2.96%, rgba(0, 0, 0,0.5) 100%), url("https://cloud-jk5wc8hg0-hack-club-bot.vercel.app/0aditya-prakash-v0gyal0lgie-unsplash.jpg");
@@ -91,46 +92,85 @@ const Header = ({ name, url, char }) => (
       </div>
     )}
     {name === 'epoch-ba' && (
-      <p className="post-text">
-        <img src="https://cloud-mx87swj8o-hack-club-bot.vercel.app/0epoch-ba-header.png" width="400px" />
-        <style>{`
-        .nav {
-          color: #fff;
-          background: #FF4794;
-        }
-        .nav-link {
-          color: #fff;
-        }
-      `}</style>
-      </p>
+      <div className="epoch-banner">
+        <p className="post-text">
+          <img src="https://cloud-itm512hmq-hack-club-bot.vercel.app/2epoch-ba-header.png" width="400px" />
+          <style>{`
+          .epoch-banner {
+            margin-bottom: 16px;
+            padding: 16px;
+            padding-top: 24px;
+            background-size: cover;
+            background-position: center;
+            background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.6) 2.96%, rgba(0, 0, 0,0.7) 100%), url("https://cloud-4q664pvib-hack-club-bot.vercel.app/0tyler-casey-e9a8-hhpezo-unsplash.jpg");
+          }
+          .nav {
+            color: #fff;
+            background: #FF4794;
+          }
+          .nav-link {
+            color: #fff;
+          }
+          .post-text {
+            margin-top: 0px; 
+          }
+        `}</style>
+        </p>
+      </div>
     )}
     {name === 'epoch-vt' && (
-      <p className="post-text">
-        <img src="https://cloud-mx87swj8o-hack-club-bot.vercel.app/3epoch-vt-header.png" width="400px" />
-        <style>{`
-        .nav {
-          color: #fff;
-          background: #FF4794;
-        }
-        .nav-link {
-          color: #fff;
-        }
-      `}</style>
-      </p>
+      <div className="epoch-banner">
+        <p className="post-text">
+          <img src="https://cloud-itm512hmq-hack-club-bot.vercel.app/1epoch-vt-header.png" width="400px" />
+          <style>{`
+          .epoch-banner {
+            margin-bottom: 16px;
+            padding: 16px;
+            padding-top: 24px;
+            background-size: cover;
+            background-position: center;
+            background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.6) 2.96%, rgba(0, 0, 0,0.7) 100%), url("https://cloud-nmiyxhmmn-hack-club-bot.vercel.app/0kevin-davison-iy_p_o1xfii-unsplash.jpg");
+          }
+          .nav {
+            color: #fff;
+            background: #FF4794;
+          }
+          .nav-link {
+            color: #fff;
+          }
+          .post-text {
+            margin-top: 0px; 
+          }
+        `}</style>
+        </p>
+      </div>
     )}
     {name === 'epoch-tx' && (
-      <p className="post-text">
-        <img src="https://cloud-mx87swj8o-hack-club-bot.vercel.app/2epoch-tx-header.png" width="400px" />
-        <style>{`
-        .nav {
-          color: #fff;
-          background: #FF4794;
-        }
-        .nav-link {
-          color: #fff;
-        }
-      `}</style>
-      </p>
+      <div className="epoch-banner">
+        <p className="post-text">
+          <img src="https://cloud-itm512hmq-hack-club-bot.vercel.app/0epoch-tx-header.png" width="400px" />
+          <style>{`
+          .epoch-banner {
+            margin-bottom: 16px;
+            padding: 16px;
+            padding-top: 24px;
+            background-size: cover;
+            background-position: center;
+            background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.6) 2.96%, rgba(0, 0, 0,0.7) 100%), url("https://cloud-cdrga84h7-hack-club-bot.vercel.app/0cosmic-timetraveler-c7jn_0ewvwq-unsplash.jpg");
+          }
+          .nav {
+            color: #fff;
+            background: #FF4794;
+          }
+          .nav-link {
+            color: #fff;
+          }
+          .post-text {
+            margin-top: 0px; 
+          }
+        `}</style>
+        </p>
+      </div>
     )}
     {name === 'gamelab' && (
       <>
@@ -311,7 +351,11 @@ export const getStaticPaths = async () => {
     'musical_note',
     'robot_face',
     'birthday',
-    'gamelab'
+    'gamelab',
+    'epoch',
+    'epoch-ba',
+    'epoch-vt',
+    'epoch-tx'
   ]
   const paths = names.map(emoji => ({ params: { emoji } }))
   return { paths, fallback: true }
