fe5fa6318605395974a3ba88115c71e2084be3de
Sam Poder <39828164+sampoder@users.noreply.github.com>
2023-03-20 14:04:27 +0800

    Revert "Update posts.js"
    This reverts commit 788064ea9a5015bda53c52224b5a2e757ed09093.

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -20,6 +20,9 @@ export const getRawPosts = async (max = null, params = {}, api = false) => {
   }
   if (max) opts.take = max
   const updates = await prisma.updates.findMany(opts)
+  if (api) {
+    let updatesWith
+  }
   return updates
 }
 
@@ -81,7 +84,8 @@ export const getPosts = async (max = null, api = false) => {
 
 export default async (req, res) => {
   const posts = await getPosts(
-    req.query.max ? Number(req.query.max) : 200
+    req.query.max ? Number(req.query.max) : 200,
+    true
   )
   return res.json(posts)
 }
