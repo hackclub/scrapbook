6220a7eea4cab19a0912e3aca565f24cf3b7f8a3
Sam Poder <sam@hackclub.com>
2023-03-20 13:43:42 +0800

    Update index.js

--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -1,8 +1,18 @@
 import prisma from '../../../lib/prisma'
+import { transformProfile } from './[username]/index.js'
 
 export const getRawUsers = (onlyFull = false, where = undefined) =>
   prisma.accounts.findMany({ where })
 
 // Find users with at least one Scrapbook post
 export default async (req, res) =>
-  getRawUsers(false).then(u => res.json(u.map(x=> ({...x, email: null})) || []))
+  getRawUsers(
+    false,        
+    {
+      NOT: {
+        updates: {
+          none: {}
+        }
+      }
+    }
+  ).then(u => res.json(u.map(x => transformProfile(x) || []))
