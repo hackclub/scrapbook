13b1beb39795c1ff7284b07ccf50434d706a87a5
Sam Poder <39828164+sampoder@users.noreply.github.com>
2023-04-07 23:14:57 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Toaster } from 'react-hot-toast'
 import { SessionProvider } from 'next-auth/react'
 import { useRouter } from 'next/router'
