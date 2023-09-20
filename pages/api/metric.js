4ab7d47b2b3818a6614098f7d0995743877cabbf
JosiasAurel <ndjosiasaurel@gmail.com>
2023-09-20 15:54:29 +0100

    use only api fn name

--- pages/api/metric.js
+++ pages/api/metric.js
@@ -4,7 +4,7 @@ export default async function pushMetrics(req, res) {
   const { time, metricKey } = req.body;
   console.log("sending metric ", metricKey, time);
 
-  metrics.timing(metricKey, time);
+  metrics.timing(metricKey.split(".")[1], time);
   metrics.increment(metricKey, 1);
   return res.json({ success: true });
 }
