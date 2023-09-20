30db941352133bf6147a9d5cd253a7182e9b2856
JosiasAurel <ndjosiasaurel@gmail.com>
2023-09-20 15:48:45 +0100

    feat: time api response

--- pages/api/metric.js
+++ pages/api/metric.js
@@ -1,9 +1,10 @@
 import metrics from "../../metrics";
 
 export default async function pushMetrics(req, res) {
-  const { metricKey } = req.body;
-  console.log("sending metric ", metricKey);
+  const { time, metricKey } = req.body;
+  console.log("sending metric ", metricKey, time);
 
+  metrics.timing(metricKey, time);
   metrics.increment(metricKey, 1);
   return res.json({ success: true });
 }
