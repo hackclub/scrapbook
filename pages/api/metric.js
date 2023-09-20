c7bafe6d94f7d6760b6f2db92eb5b1686236fb65
JosiasAurel <ndjosiasaurel@gmail.com>
2023-09-20 16:29:03 +0100

    update /api/metrics

--- pages/api/metric.js
+++ pages/api/metric.js
@@ -2,9 +2,14 @@ import metrics from "../../metrics";
 
 export default async function pushMetrics(req, res) {
   const { time, metricKey } = req.body;
-  console.log("sending metric ", metricKey, time);
 
-  metrics.timing(metricKey.split(".")[1], time);
+  if (time) {
+    metrics.timing(metricKey.split(".")[1], time);
+    console.log(metricKey.split(".")[1], time);
+    return res.json({ success: true });
+  }
+
+  console.log("sending metric ", metricKey);
   metrics.increment(metricKey, 1);
   return res.json({ success: true });
 }
