2d2cf00f50e7abe1dd4abaea720c72ca64956d30
JosiasAurel <ndjosiasaurel@gmail.com>
2024-07-31 17:35:22 +0100

    change image upload endpoint

--- pages/api/presigned-s3.js
+++ pages/api/presigned-s3.js
@@ -1,18 +1,10 @@
 import { v4 as uuidv4 } from "uuid";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
-import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { PutObjectCommand } from "@aws-sdk/client-s3";
+import s3 from "../../lib/s3";
 
 export default async function handleGetSignedUrl(req, res) {
   const { filename, filetype } = req.query;
-  console.log(filename, filetype)
-
-  const s3Client = new S3Client({
-    region: "us-east-1",
-    credentials: {
-      accessKeyId: process.env.NEXT_PUBLIC_S3_ACCESS_KEY_ID,
-      secretAccessKey: process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY
-    }
-  });
 
   const command = new PutObjectCommand({
     Bucket: 'scrapbook-into-the-redwoods',
@@ -21,7 +13,7 @@ export default async function handleGetSignedUrl(req, res) {
   });
 
   // generate a new signed url that expires in 1 hour
-  const signedUrl = await getSignedUrl(s3Client, command, { expiresIn: 3600 })
+  const signedUrl = await getSignedUrl(s3, command, { expiresIn: 3600 })
 
   if (signedUrl) return res.send({ signedUrl })
 
