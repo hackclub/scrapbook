101b817b371e1ff8a7ef8cb07fb2866c4d4d2f8a
JosiasAurel <ndjosiasaurel@gmail.com>
2024-08-27 10:43:23 +0100

    remove specific usernames from metric key

--- pages/api/metric.js
+++ pages/api/metric.js
@@ -5,11 +5,9 @@ export default async function pushMetrics(req, res) {
 
   if (time) {
     metrics.timing(metricKey, time);
-    console.log(metricKey, time);
     return res.json({ success: true });
   }
 
-  console.log("sending metric ", metricKey);
   metrics.increment(metricKey, 1);
   return res.json({ success: true });
 }
